8a492ff10389bc0062a0962c57ae86d1ed6b9fd8
michael_klimenko_ <mklimenko@gmail.com>
2016-11-01 01:12:23 +0200

    used json filter as reference

--- lib/logstash/filters/geocoding.rb
+++ lib/logstash/filters/geocoding.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'logstash/filters/base'
 require 'logstash/namespace'
+require "logstash/json"
 require 'rest-client'
 
 # This example filter will replace the contents of the default
@@ -13,63 +14,104 @@ class LogStash::Filters::Geocoding < LogStash::Filters::Base
   # configure this filter from your Logstash config.
   #
   # filter {
-  #   example {
-  #     message => "My message..."
+  #   geocoding {
   #   }
   # }
   #
   config_name "geocoding"
 
-  # Replace the message with this value.
-  config :message, :validate => :string, :default => "test 123"
+  # The configuration for the JSON filter:
+  # [source,ruby]
+  #     source => source_field
+  #
+  # For example, if you have JSON data in the `message` field:
+  # [source,ruby]
+  #     filter {
+  #       json {
+  #         source => "message"
+  #       }
+  #     }
+  #
+  # The above would parse the json from the `message` field
+  config :source, :validate => :string, :required => true
 
+
+  # Define the target field for placing the parsed data. If this setting is
+  # omitted, the JSON data will be stored at the root (top level) of the event.
+  #
+  # For example, if you want the data to be put in the `location` field:
+  # [source,ruby]
+  #     filter {
+  #       geocoding {
+  #         target => "location"
+  #       }
+  #     }
+  #
+  # JSON in the value of the `source` field will be expanded into a
+  # data structure in the `target` field.
+  #
+  # NOTE: if the `target` field already exists, it will be overwritten!
+  config :target, :validate => :string
+
+  # Append values to the `tags` field when there has been no
+  # successful match
+  config :tag_on_failure, :validate => :array, :default => ["_jsonparsefailure"]
+
+  # Allow to skip filter on invalid json (allows to handle json and non-json data without warnings)
+  config :skip_on_invalid_json, :validate => :boolean, :default => false
 
   public
   def register
-    # Add instance variables
+    # Nothing to do here
   end # def register
 
   public
   def filter(event)
+    @logger.debug? && @logger.debug("Running geolocation filter", :event => event)
 
-    if @message
+    source = event.get(@source)
+    return unless source
 
+    begin
+      parsedSource = LogStash::Json.load(source)
+    rescue => e
+      unless @skip_on_invalid_json
+        @tag_on_failure.each{|tag| event.tag(tag)}
+        @logger.warn("Error parsing json", :source => @source, :raw => source, :exception => e)
+      end
+      return
+    end
+
+    if @target
       response = RestClient.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAnWuLpHCsyykK0Z6Is1sZeYHGr8HcZrMs",
-          { 'cellTowers' => [
-              {
-                  'cellId' => 32446,
-                  'locationAreaCode' => 56964,
-                  'mobileCountryCode' => 310,
-                  'mobileNetworkCode' => 410
-              }
-          ]}.to_json, {content_type: :json, accept: :json})
+                                 # { 'cellTowers' => [
+                                 #     {
+                                 #         'cellId' => 32446,
+                                 #         'locationAreaCode' => 56964,
+                                 #         'mobileCountryCode' => 310,
+                                 #         'mobileNetworkCode' => 410
+                                 #     }
+                                 # ]}.to_json,
+                                 parsedSource,
+                                 {content_type: :json, accept: :json})
 
       @logger.debug? && @logger.debug("Response : #{response}")
-      # using the event.set API
-      parsed = JSON.parse(response.body)
-      parsed.each{|k, v| event.set(k, v)}
 
-      # if locObj.location
-      #   # event.set(lat,parsed.location.lat)
-      #   # event.set(lng,parsed.location.lng)
-      #   # event.set(accuracy,locObj.accuracy)
-      # end
+      parsedTarget = LogStash::Json.load(response.body)
+      event.set(@target,parsedTarget)
+      # parsedTarget.each{|k, v| event.set(@target.k, v)}
 
-      # if locObj.error
-      #   event.set(error,locObj.error.message)
-      # end
+    else
+      unless parsedTarget.is_a?(Hash)
+        @tag_on_failure.each{|tag| event.tag(tag)}
+        @logger.warn("Parsed JSON object/hash requires a target configuration option", :source => @source, :raw => source)
+        return
+      end
 
-
-
-      # Replace the event message with our message as configured in the
-      # config file.
-      event.set(message, @message)
-      # correct debugging log statement for reference
-      # using the event.get API
-      @logger.debug? && @logger.debug("Message is now: #{event.get("message")}")
-    end
+    @logger.debug? && @logger.debug("Message is now: #{event.get(@target)}")
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
+    end # if #target
   end # def filter
-end # class LogStash::Filters::Example
+end # class LogStash::Filters::Geocoding
